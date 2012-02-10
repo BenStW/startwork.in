990b1188adecdb26113f31258d2858814cdc7be4
Benedikt Voigt <mail@benedikt-voigt.net>
2012-02-10 15:13:50 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartWork::Application.config.secret_token = '32e7566bd7ed6692faa94486ff09544e682744af97432927013862b3a9b695767ada5bafe628edc76d02188f1ef2f5d8b2d3135fb7f5886e06e3397546585320'
---
+++ test/functional/store_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
