9367cdbc05a5a6274ac8739370d22d42ef8c27bd
Benedikt Voigt <mail@benedikt-voigt.net>
2012-03-10 23:32:34 +0100

    added load path for I18N

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module StartWork
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    I18n.load_path  += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
