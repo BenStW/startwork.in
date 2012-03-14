fe84e054e30007db4a31f1ff795085e02842945f
Benedikt Voigt <mail@benedikt-voigt.net>
2012-03-14 20:54:14 +0100

    fall back to en when german translation cant be loaded

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module StartWork
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     #I18n.load_path  += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = [:en]
+    
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
