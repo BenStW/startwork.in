d4a5453b57120a1d9202c17df86dcf2845e15d10
Benedikt Voigt <mail@benedikt-voigt.net>
2012-03-15 07:49:47 +0100

    touched lang file

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module StartWork
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     #I18n.load_path  += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :en
-    config.i18n.fallbacks = [:en]
     
 
     # Configure the default encoding used in templates for Ruby 1.9.
