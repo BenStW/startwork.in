c92212cbe6591ff4f0a3524cb1ed823368df78bb
Benedikt Voigt <mail@benedikt-voigt.net>
2012-03-22 16:36:38 +0100

    removed limit of friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,10 +7,10 @@ class FriendshipsController < ApplicationController
   end
   
   def create
-    if current_user.friendships.count>2
-      flash[:alert] = "Currently you can have only 3 friends."
-      redirect_to friendships_url
-    else      
+ #  if current_user.friendships.count>2
+ #    flash[:alert] = "Currently you can have only 3 friends."
+ #    redirect_to friendships_url
+ #  else      
       @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
       @inverse_friendship = current_user.inverse_friendships.build(:user_id => params[:friend_id])
      
@@ -21,7 +21,6 @@ class FriendshipsController < ApplicationController
         flash[:alert] = "Unable to add friend."
         redirect_to friendships_url
       end
-    end
   end
 
   def destroy
