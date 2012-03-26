2a1b4db69ced337ee50afcd4457818382209a5b7
Benedikt Voigt <mail@benedikt-voigt.net>
2012-03-26 18:13:22 +0200

    fixed referer bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :activated
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :activated, :referer
   
   validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
   #validates :room, presence: true
