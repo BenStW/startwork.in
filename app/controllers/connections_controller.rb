0228fb54bbea00cfa9bd704586969c5017271a46
Benedikt Voigt <mail@benedikt-voigt.net>
2012-05-31 09:40:21 +0200

    precompiled assets

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,8 +3,10 @@ class ConnectionsController < ApplicationController
   def start
     logger.info "start connections of user_ids #{params[:user_ids].to_yaml}"
     for user_id in params[:user_ids]
-       @user = User.find(user_id)
-       @user.start_connection
+      if user_id >1
+         @user = User.find(user_id)
+         @user.start_connection
+      end
     end
     render :json => "succussfully started connection"
   end
@@ -12,8 +14,10 @@ class ConnectionsController < ApplicationController
   
   def end    
     for user_id in params[:user_ids]
-       @user = User.find(user_id)
-       @user.end_connection
+      if user_id >1      
+        @user = User.find(user_id)
+        @user.end_connection
+     end
     end
     render :json => "succussfully ended connection"
   end
