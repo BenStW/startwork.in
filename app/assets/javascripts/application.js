1114af398bff9520283d1be87b90e2c3ce60af2b
BenStW <benedikt@startwork.in>
2012-06-26 21:47:40 +0200

    precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -27,35 +26,6 @@
 // require jquery_plugins/jquery.popupWindow - not needed yet
 // require twitter/bootstrap - not needed
 // require twitter/bootstrap.min - not needed
-=======
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-// require jquery_ujs - needed?
-//= require jquery-ui
-//= require jquery_plugins/jquery-ui-i18n
-//= require jquery_plugins/jquery.jplayer.min
-//= require jquery_plugins/jquery.weekcalendar
-//= require twitter/bootstrap-dropdown
-//= require twitter/bootstrap-modal
-//= require twitter/bootstrap-tooltip
-//= require twitter/bootstrap-popover
-//= require twitter/bootstrap-transition
-// require tokbox/OT_LayoutContainer
-//= require_tree . 
-// require jquery_plugins/jquery.popupWindow - not needed yet
-// require twitter/bootstrap - not needed
-// require twitter/bootstrap.min - not needed
 //= require woothemes-flexslider/jquery.flexslider
 //= require woothemes-flexslider/jquery.flexslider-min
->>>>>>> 65f0ebd5ab50017c1f64efc0886e2ea02664101e
+
