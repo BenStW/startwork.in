93db7e2ebe9cea3f5d50bec22a93932dfd2b555d
BenStW <benedikt@startwork.in>
2012-07-20 19:04:51 +0200

    added after_creation_of_appointment mail

--- app/mailers/start_work_mailer.rb
+++ app/mailers/start_work_mailer.rb
@@ -1,11 +1,29 @@
 class StartWorkMailer < ActionMailer::Base
+  layout 'mail_layout'
   default from: "info@startwork.in" # Sender is defined in the environment files
   
   def after_registration(user)
     @user = user
-    mail(:to => [user.email], 
+    mail(:to => user.email, 
     :bcc => ["benedikt@startwork.in","miro@startwork.in", "robert@startwork.in"],
     :subject => "Willkommen auf StartWork!") 
   end 
+  
+  def after_creation_of_appointment(appointment)
+    @user = appointment.user
+    @appointment = appointment
+    @users =  User.users_tomorrow
+    
+    [55,108,523,526,846].each do |user_id|
+      if u=User.find_by_id(user_id)
+        @users<<u
+      end
+    end
+    @users = @users.uniq
+
+    mail(:to => @user.email, 
+    :bcc => ["benedikt@startwork.in","miro@startwork.in", "robert@startwork.in"],
+    :subject => "Willkommen auf StartWork!") 
+  end  
 
 end
